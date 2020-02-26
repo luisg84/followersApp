d2f1a206791d41ea49051bead5d6cb80d1101d8e
Luis Sanchez <luissanchez@MacBook-Pro-de-Luis.local>
2020-02-26 13:14:40 -0600

    previo instal cocoap

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:followers_app/src/Followers/bloc/bloc_followers.dart';
 import 'package:followers_app/src/Followers/ui/screens/home.dart';
 import 'package:followers_app/src/Followers/ui/screens/login_page.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 
 import 'src/Followers/ui/screens/register_page.dart';
 
@@ -9,12 +11,14 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FollowersApp',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: LoginPage(),
-      ),
-    );
+    return BlocProvider(
+        child: MaterialApp(
+          title: 'FollowersApp',
+          debugShowCheckedModeBanner: false,
+          home: Scaffold(
+            body: LoginPage(),
+          ),
+        ),
+        bloc: FollowersBloc());
   }
 }
---
+++ lib/src/Pacientes/bloc/bloc_pacientes.dart
@@ -0,0 +1,8 @@
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+
+class PacientesBloc implements Bloc {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+}
